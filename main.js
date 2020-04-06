13ab872ddaca6739a0a9ff6dacf7cca14cb9fd9f
albatros82 <albatros82@inbox.ru>
2020-04-06 19:20:49 +0700

    060420

--- main.js
+++ main.js
@@ -1,3 +1,90 @@
+// встроенные функции и методы:
+
+// .toString(); - явное преобразование в строку
+// id.toString();
+
+// Math.trunc - встроенная функция, которая удаляет десятичную часть
+// Math.trunc(49.2);
+
+// Object.assign(dest, [src1, src2, src3...]) // Клонирует или копирует объект и объекты а также Symbol
+// let clone = Object.assign( {}, user ); // клонируем user в clone
+// let clone = Object.assign( clone, user, user2 ); // копируем значения из user и user2 в clone 
+
+
+
+// this
+// Функцию, которая является свойством объекта, называют методом этого объекта.
+
+
+
+
+
+
+// Символы
+
+// let id = Symbol('имя - описание');
+
+// Symbol.for(key) // создание глобальных символов
+// let id = Symbol.for("имя - описание"); // если символа не существует, он будет создан
+
+// let id = Symbol('имя - описание')
+// let user = {[id]: 1}
+// alert( id.description ); // имя - описание для любых символов (глобальных и локальных)
+// alert(user[id]) // 1
+
+
+// Symbol.keyFor(sym), наоборот, принимает глобальный символ и возвращает его имя - описание.
+// let id = Symbol.for('имя - описание');
+// alert( Symbol.keyFor(id) );// имя - описание
+// Если символ неглобальный, метод не сможет его найти и вернёт undefined
+
+
+
+// Объекты
+
+// Чтобы получить доступ к свойству, мы можем использовать:
+// Запись через точку: obj.property.
+// Квадратные скобки obj["property"]. Квадратные скобки позволяют взять ключ из переменной, например, obj[varWithKey].
+// Удаление свойства: delete obj.prop.
+// Проверка существования свойства: "key" in obj.
+// Перебор свойств объекта: цикл for for (let key in obj).
+
+// 'key' in obj
+// let key = 'name';
+// alert ( key in user ); // выдаст true если есть свойство name в объекте user
+// alert ( 'name' in user ) // обращаемся к свойству через двойные ковычки если это не переменная как выше
+
+
+// Цикл «for…in»
+// при переборе не находит Symbol();
+
+// for (let key in user) {
+//     console.log (key); // ключи
+//     console.log ( user[key] ); // зачение ключей
+// }
+
+
+// Глубокое клонирование (объект с вложенным объектом)
+
+// пример: 
+// let user = {
+//     name: 'Ivan',
+//     sizes: {
+//         height: 172,
+//         width: 52,
+//     }
+// }
+// alert( user.sizes.height);
+
+// Существует стандартный алгоритм глубокого клонирования, 
+// Structured cloning algorithm. Он решает описанную выше задачу, 
+// а также более сложные задачи. Чтобы не изобретать велосипед, 
+// мы можем использовать реализацию этого алгоритма из 
+// JavaScript-библиотеки lodash, метод _.cloneDeep(obj).
+
+
+
+// Функции:
 
 // // Function Expression
 // let sum = function(a, b) {
@@ -36,6 +123,10 @@
 // из цикла можно выйти директивой break;
 // итерацию цикла можно пропустить директивой continue;
 // с тернарным оператором ? вызовет ошибку!
+
+
+
+// Операторы:
 
 // цепочка ИЛИ "||" возвращает первое истинное значение или последнее, если такое значение не найдено.
 // цепочка И "&&" возвращает первое ложное значение или последнее, если все значения были истиными.
@@ -521,3 +612,376 @@ Boolean ( str ); булево значение
 
 // let sayHello = (name, age) => `${sayHi} ${name}, ваш возраст ${age} ${year}.`;
 // alert(sayHello(name, age));
+
+// let age = prompt ('Сколько Вам лет','10');
+
+// function welcome (age) {
+//     return age < 10 ? 'Привет': 'Здравствуйте';
+// }
+
+// alert ( welcome() );
+
+
+// let age = prompt ('Сколько Вам лет','10');
+
+// let welcome = (age < 10) ?
+//     () => 'Привет':
+//     () => 'Здравствуйте';
+
+// let typeWelcome = (typeof welcome == 'function') ? 'welcome является функцией' : 'welcome не является функцией';
+// alert (typeWelcome); 
+
+
+// Объекты
+
+// let user = {
+//     name: 'John',
+//     age: 25
+// }
+
+// user.isAdmin = true;
+// alert( user.age );
+// alert ( user.isAdmin );
+// delete user.age;
+// user.age === undefined ? alert ('удалили свойство age') : alert('проверка не прошла');
+
+// let user = {};
+// user['likes birds'] = true;
+// alert ( user ['likes birds'] );
+
+// let key = 'likes birds';
+// user[key] = true;
+// alert ( user[key] );
+
+// let user = {
+//     name: 'Joshua',
+//     age: 55,
+// };
+
+// let key = prompt ('Что вы хотите узнать о пользователе', 'age');
+// alert ( user[key] );
+
+// let fruit = prompt ('Какой фрукт купить','apple');
+
+// let bag = {
+//     [fruit]: 8,
+//     veg: 3,
+// }
+
+// alert ( bag.apple );
+// alert ( bag['veg'] );
+
+// let fruit = 'apple';
+// let bag = {
+//     [fruit + 'Computers']: 5 // bag.appleComputers = 5
+// };
+
+// function makeUser (name, age, sex) {
+//     return {
+//         name: name,
+//         age: age,
+//         sex: sex,
+//     }
+// }
+
+// let user= makeUser ('John', 25, 'men');
+// alert ( user.name + ' ' + user.age + ' ' + user.sex);
+
+// let obj = {
+//   0: "Тест" // то же самое что и "0": "Тест"
+// };
+
+// // обе функции alert выведут одно и то же свойство (число 0 преобразуется в строку "0")
+// alert( obj["0"] ); // Тест
+// alert( obj[0] ); // Тест (то же свойство)
+
+
+// let user = {};
+// alert ( user.noSuchProperty === undefined ); // обращаемся к несуществующему свойству
+
+// let user = {
+//     name: 'Lira', 
+//     age: 22,
+//     sex: 'women',
+// }
+
+// let key = prompt ('Проверка на существование свойства', 'name')
+// let output = key in user ? ('Свойство найдено') : 'Свойство не найдено';
+// alert ( output );
+
+// let user = {
+//     name: 'John',
+//     age: 20,
+// }
+
+// alert ( 'name' in user );
+
+// let user = {
+//     name: 'John',
+//     age: 25,
+//     isAdmin: true,
+// }
+
+// for (let key in user) {
+//     // ключи
+//     console.log (key);
+//     //значение ключей
+//     console.log ( user[key] );
+// }
+
+// целочисленные имена выводятся по возрастанию, не целочисленные по созданию
+// let codes = {
+//     "49": "Германия",
+//     "41": "Швейцария",
+//     "44": "Великобритания",
+//     // ..,
+//     "1": "США"
+// }
+
+// let codes = {
+//     49: "Германия",
+//     "+41": "Швейцария",
+//     "+44": "Великобритания",
+//     // ..,
+//     "+1": "США"
+// }
+
+
+// for (let code in codes) {
+//     alert ( +code + ' ' + codes[code]);
+// }
+
+// alert ( String(Math.trunc(Number('49'))) );
+// alert ( String(Math.trunc(Number('+49'))) );
+// alert ( String(Math.trunc(Number('1.6'))) );
+
+// let user = {};
+
+// user.name = 'NickName';
+// user.age = 22;
+// user.sex = 'men';
+// user['dominant hand'] = 'left';
+
+// for (let key in user) {
+//     console.log ( key + ':' + ' ' + user[key] );
+// }
+
+// let key = prompt ('', 'name')
+// alert (key in user);
+
+// const user = {
+//     name: 'Ivan',
+//     age: 25,
+// }
+
+// for (let key in user) {
+//     alert ( key + ':' + ' ' + user[key] );
+// }
+
+// let user = {
+//     name: "John",
+//     age: 30
+// };
+
+// let clone = {};
+// for (let key in user) {
+//     clone[key] = user[key]
+// }
+
+// clone.name = 'Ivan';
+// alert (clone.name);
+// alert (user.name);
+
+// let user = {name: 'John'};
+// let permissions1 = { canView: true };
+// let permissions2 = { canEdit: true };
+
+// // копируем все свойства из Permissons1 и 2 в user
+// Object.assign( user, permissions1, permissions2 );
+
+// for (let key in user) {
+//     if (key == 'name') continue;
+//     alert ( key + ':' + ' ' + user[key] );
+// }
+
+//добавление и перезапись свойств
+// let user = {name: 'John'}
+// Object.assign( user, {name: 'Ivan', ['like vodka']: true, isAdmin: true,} );
+// // for (let key in user) {
+// //     alert ( key + ':' + ' ' + user[key] );
+// // }
+
+// клонирование объектов
+// let clone = Object.assign( {}, user ); 
+
+// for (let key in clone) {
+//     alert ( key + ':' + ' ' + clone[key] );
+// }
+
+
+// Нужно применить метод глубокого клонирования в объекте есть вложенные объекты
+// мы можем использовать реализацию этого алгоритма из JavaScript-библиотеки lodash, метод _.cloneDeep(obj).
+// let user = {
+//     name: 'Ivan',
+//     sizes: {
+//         height: 172,
+//         width: 52,
+//     }
+// }
+// alert( user.sizes.height);
+
+// let user = {};
+// user.name = 'John';
+// user.surname = 'Smith';
+// user.name = 'Pete';
+// delete user.name;
+
+// let shedule = {
+//     // name: 'John',
+//     // age: 25,
+//     // sex: 'men',
+// };
+
+// function isEmpty (obj) {
+//     let num = 0;
+//     for (let key in obj) {
+//         num++;
+//     }
+//     if (num == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// alert ( isEmpty(shedule) );
+
+// shedule['8:30'] = 'get up';
+
+// alert ( isEmpty(shedule) );
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+
+// let sum = 0;
+// for (let key in salaries) {
+//     sum += salaries[key];
+// }
+
+// alert (sum);
+
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+// };
+
+// function multiplyNumeric (obj) {
+//     for (let key in obj) {
+//         if ( typeof (menu[key]) == 'number' ) {
+//             obj[key] = obj[key] * 2;
+//         }
+//     }
+// }
+
+// for (let key in menu) {
+//     console.log (key + ':' + ' ' + menu[key])
+// }
+
+// multiplyNumeric (menu);
+
+// for (let key in menu) {
+//     console.log (key + ':' + ' ' + menu[key])
+// }
+
+
+// Тип данных Symbol
+
+// let id = Symbol('id');
+// // .toString() - явное преобразование в строку
+// alert( id.toString() ); // Symbol('id');
+
+// .symbol.description вывести только описание
+// alert( id.description ); // id
+
+
+// let id = Symbol('identification');
+
+// let user = {
+//     name: 'Ivan',
+//     [id]: 1,
+// }
+
+// alert ( user[id] ); // значение
+// alert ( id.description ); // описание
+
+// let id = Symbol.for('id');
+
+// let idAgain = Symbol.for('id');
+
+// alert (id == idAgain);
+
+// let id = Symbol.for('identification');
+// alert( Symbol.keyFor(id) );// identification
+
+// let globalSymbol = Symbol.for('name'); // глобальный символ
+// let localSymbol = Symbol('name'); // локальный символ
+
+// alert( Symbol.keyFor(globalSymbol) ); // name, глобальный символ
+// alert( Symbol.keyFor(localSymbol) ); // undefined для неглобального символа
+
+// alert ( globalSymbol.description ); // name
+// alert ( localSymbol.description ); // name
+
+
+
+//Методы объекта 'this'
+//Функцию, которая является свойством объекта, называют методом этого объекта.
+
+// let user = {
+//     name: 'John',
+//     age: 25,
+//     sayHi: function () {return 'привет ' + this.name},
+// }
+
+// alert ( user.sayHi() );
+
+// let user = {name: 'John'};
+// let admin = {name: 'Admin'};
+
+// function sayHi() {
+//     return alert( 'Привет ' + this.name );
+// }
+
+// user.sayHi = sayHi;
+// admin.sayHi = sayHi;
+
+// user.sayHi();
+// admin.sayHi();
+
+// for (let key in user) {
+//     console.log (key + ':' + ' ' + user[key]);
+// }
+
+let user1 = {
+    name: 'John',
+    age: 25,
+    dateOfBirht: '25.10.2019',
+}
+
+for (let key in user1) {
+    console.log( user1[key] );
+}
+
+function updateDate (dateOfBirht) {
+    this.dateOfBirht = dateOfBirht;
+}
+
+user1.f = updateDate;
+user1.f('24.10.2018');
+
+for (let key in user1) {
+    console.log( user1[key] );
+}
