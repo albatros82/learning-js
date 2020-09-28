98e262c02558ab56dfeee8df3ed4e90bcaf5eb30
albatros82 <albatros82@inbox.ru>
2020-09-28 10:28:36 +0700

    280920

--- main.js
+++ main.js
@@ -3781,13 +3781,13 @@ Boolean ( str ); булево значение
 
 // число фибоначи через перебор (итерации цикла)
 
-function fib(n) {
-    let arr = [1, 1];
-    while( arr.length < n ) {
-        arr.push( arr[arr.length - 1] + arr[arr.length - 2] );
-    }
-    // return arr[arr.length - 1];
-    return arr;
-}
+// function fib(n) {
+//     let arr = [1, 1];
+//     while( arr.length < n ) {
+//         arr.push( arr[arr.length - 1] + arr[arr.length - 2] );
+//     }
+//     // return arr[arr.length - 1];
+//     return arr;
+// }
 
-console.log( fib(7) );
+// console.log( fib(7) );
